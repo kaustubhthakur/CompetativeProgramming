0540a3ca5aac17cbe52fc7e903c9f90787909640
Kaustubh <kaustubhthakur76@gmail.com>
2023-02-19 17:08:10 +0530

    specialist commit

--- Specialist/problems/0010.cpp
+++ Specialist/problems/0010.cpp
@@ -10,6 +10,15 @@ while(t--)
 {
     int n,m;
     cin>>n>>m;
+    if(n==1 && m==1)
+    {
+        cout<<0<<endl;
+    }
+   else
+    {
+        int ans = (n-1)+(m-1) +min(n,m);
+        cout<<ans<<endl;
+    }
 }
 
 
