8d58aa222046ec68f477bb9c7b9f9a4c7f9f2533
Kaustubh <kaustubhthakur76@gmail.com>
2023-02-21 16:42:55 +0530

    specialist commit

---
+++ Specialist/problems/0011.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+
+   int n;
+   cin>>n;
+   int ans = pow(2,n);
+   cout<<ans-1<<endl;
+}
+int main()
+{
+int t;
+cin>>t;
+while(t--)
+{
+    solve();
+}
+
+return 0;
+
+}
---
+++ Specialist/problems/0013.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+
+ int n;
+ cin>>n;
+ vector<int>a(n);
+ for(int i=0;i<n;i++) cin>>a[i];
+
+}
+int main()
+{
+
+
+int t;
+cin>>t;
+while(t--)
+{
+    solve();
+}
+
+
+}
